6d6ea7bf63bd6b6c2c6cc23eb27a6c64ee183654
Clemerson da Silva <francisco.pereira77510@alunos.ufersa.edu.br>
2024-12-12 20:26:35 -0300

    first commit

---
+++ database/pdo.php
@@ -0,0 +1,7 @@
+
+<?php
+$pdo = new PDO('mysql:dbname=urubuDoPix;host=mysql', 'root', 'miguel30', [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]);
+return ;
+?>
+
+
---
+++ script-sql/init.sql
@@ -0,0 +1,16 @@
+CREATE database IF NOT EXISTS urubuDoPix;
+use urubuDoPix; 
+
+create table if not exists users(
+    id int auto_increment primary key,
+    user_name varchar(35) not null,
+    balance float not null
+);
+
+create table if not exists transactions(
+    id int auto_increment primary key,
+    userId int,
+    depositValue float not null, 
+    depositDate date not null, 
+    FOREIGN KEY (userId) REFERENCES users(id)
+);
