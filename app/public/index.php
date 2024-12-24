c2c705d0245eb5cfc5f584e1d5f6fbb96093bc78
Clemerson da Silva <francisco.pereira77510@alunos.ufersa.edu.br>
2024-12-24 15:41:55 -0300

    criacao de funcionalidade para listar todos os funcionários - nao vou usar paginacao por se tratar de uma aplicacao pequena

--- app/public/index.php
+++ app/public/index.php
@@ -1,17 +1,15 @@
 <?php
 require_once "../vendor/autoload.php";
 require_once "/app/routes/routes.php";
-var_dump(file_exists('../src/database/pdo.php'));
+/* var_dump(file_exists('../src/database/pdo.php'));
 var_dump(class_exists('App\database\Database'));
-
+ */
 use App\controllers\userController;
 
 $obj = new userController();
-$obj->Use();
 
 try {
    $url = parse_url($_SERVER['REQUEST_URI'])['path'];
-   var_dump($url);
    $request = $_SERVER['REQUEST_METHOD'];
    if(!isset($router[$request])){
       throw new Exception("A routa não existe");
