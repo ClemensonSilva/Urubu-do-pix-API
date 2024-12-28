b9b43e521c798e0eec74914b0aac179ec142d943
ClemensonSilva <clemesonsilva736@gmail.com>
2024-12-28 15:09:35 -0300

    Updated SQL schema with foreign key constraints

--- app/src/models/transactionModel.php
+++ app/src/models/transactionModel.php
@@ -7,14 +7,15 @@ use App\controllers\userController;
 use PDO;
 use DateTime;
 
-require_once '../src/database/pdo.php'; // isso é temporario ate ajustar os namespaces completamente
+require_once "../src/database/pdo.php"; // isso é temporario ate ajustar os namespaces completamente
 use PDOException;
 use stdClass;
 
 class TransactionModel
 {
     public function createTransaction(stdClass $transactionParams)
-    { // user_id e depositValue
+    {
+        // user_id e depositValue
 
         // futuramente add codigo para coletar o id do usuario usando nome e email
         try {
@@ -24,42 +25,56 @@ class TransactionModel
             $user_id = $transactionParams->user_id;
             $depositValue = $transactionParams->depositValue;
             $depositDate = $transactionParams->depositDate;
-            $user_balance = userController::getUserInformation($user_id)->user_balance;
+            $user_balance = userController::getUserInformation($user_id)
+                ->user_balance;
 
             if (empty($depositDate)) {
                 $depositDate = TransactionModel::setDate();
             }
 
             if (empty($depositValue) || empty($user_id)) {
-                echo json_encode(['error' => 'The transaction data is mandatory']);
+                echo json_encode([
+                    "error" => "The transaction data is mandatory",
+                ]);
             } else {
                 if (TransactionModel::userhasBalance($user_id, $depositValue)) {
                     $pdo->beginTransaction();
-                    TransactionModel::addDepositInvestiment($pdo, $user_id, $depositDate, $depositValue);
+                    TransactionModel::addDepositInvestiment(
+                        $pdo,
+                        $user_id,
+                        $depositDate,
+                        $depositValue
+                    );
 
                     $newBalance = $user_balance - $depositValue;
 
-                    TransactionModel::updateUserBalance($pdo, $newBalance, $user_id);
+                    TransactionModel::updateUserBalance(
+                        $pdo,
+                        $newBalance,
+                        $user_id
+                    );
                     $pdo->commit();
-                    echo json_encode(['sucess' => 'Transaction finished corretlly']);
+                    echo json_encode([
+                        "sucess" => "Transaction finished corretlly",
+                    ]);
                 } else {
-                    echo json_encode(['error' => 'Insuficient Balance']);
+                    echo json_encode(["error" => "Insuficient Balance"]);
                 }
             }
         } catch (PDOException $e) {
             $pdo->rollBack();
-            echo json_encode(['error' => $e->getMessage()]);
+            echo json_encode(["error" => $e->getMessage()]);
         }
     }
-
-
-    public  function profitInvestiment(stdClass $transactionParams)
+    public function profitInvestiment(stdClass $transactionParams)
     {
-        // user_id, transaction_id, 
+        // user_id, transaction_id,
         $transaction_id = $transactionParams->transaction_id;
-        $transactionInfo = TransactionModel::getTransactionInfo($transaction_id);
+        $transactionInfo = TransactionModel::getTransactionInfo(
+            $transaction_id
+        );
         $user_id = $transactionParams->user_id;
-        $userInformation = (userController::getUserInformation($user_id));
+        $userInformation = userController::getUserInformation($user_id);
         unset($userInformation->user_balance);
 
         if (is_string($transactionInfo)) {
@@ -67,8 +82,8 @@ class TransactionModel
             exit();
         }
 
-        $depositDate =   $transactionInfo->depositDate;
-        $depositValue =   $transactionInfo->depositValue;
+        $depositDate = $transactionInfo->depositDate;
+        $depositValue = $transactionInfo->depositValue;
 
         $depositDate = new DateTime($depositDate);
         $now = new DateTime();
@@ -77,52 +92,67 @@ class TransactionModel
 
         $interest = 0.33;
 
-        $profit = ($depositValue * pow((1 + $interest), $days)) - $depositValue;
+        $profit = $depositValue * pow(1 + $interest, $days) - $depositValue;
         echo json_encode([
             "transaction_id" => $transaction_id,
             "user" => $userInformation,
             "profit" => $profit,
             "depositValue" => $depositValue,
             "interest" => $interest,
-            "depositDate" => $depositDate
+            "depositDate" => $depositDate,
         ]);
     }
 
-
-    public static function  addDepositInvestiment(PDO $pdo, int $user_id,  $depositDate, float $depositValue): void
-    {
-        $sql = "INSERT into transactions(userId, depositValue,depositDate) 
+    public static function addDepositInvestiment(
+        PDO $pdo,
+        int $user_id,
+        $depositDate,
+        float $depositValue
+    ): void {
+        $sql = "INSERT into transactions(userId, depositValue,depositDate)
         VALUES(:userId, :depositValue, :depositDate)";
         Database::consultingDB($pdo, $sql, [
-            ':depositValue' =>  $depositValue,
-            ':depositDate' =>  $depositDate,
-            ':user_id' => $user_id
+            ":depositValue" => $depositValue,
+            ":depositDate" => $depositDate,
+            ":user_id" => $user_id,
         ]);
     }
-    public static function updateUserBalance(PDO $pdo, float $newBalance, int $user_id): void
-    {
+    public static function updateUserBalance(
+        PDO $pdo,
+        float $newBalance,
+        int $user_id
+    ): void {
         $sql = "UPDATE users set user_balance =:newBalance where id=:user_id";
-        Database::consultingDB($pdo, $sql, [':newBalance' =>  $newBalance, ':user_id' => $user_id]);
+        Database::consultingDB($pdo, $sql, [
+            ":newBalance" => $newBalance,
+            ":user_id" => $user_id,
+        ]);
     }
 
     public static function userhasBalance(int $user_id, float $depositValue)
     {
         $balance = userController::getUserInformation($user_id)->user_balance;
-        return ($balance > $depositValue) ? true : false;
+        return $balance > $depositValue ? true : false;
     }
     public static function setDate()
     {
-        return date('y-m-d');
+        return date("y-m-d");
     }
 
-    public static function getTransactionInfo(int $transaction_id): stdClass|string
-    {
+    public static function getTransactionInfo(
+        int $transaction_id
+    ): stdClass|string {
         $pdo = new Database();
         $pdo = $pdo->getConnection();
-        $sql = 'SELECT * FROM transactions where id=:transaction_id';
-        $result = Database::consultingDB($pdo, $sql, [':transaction_id' => $transaction_id]);
+        $sql = "SELECT * FROM transactions where id=:transaction_id";
+        $result = Database::consultingDB($pdo, $sql, [
+            ":transaction_id" => $transaction_id,
+        ]);
         if (!$result) {
-            return json_encode(['error' => true, 'message' => 'Transaction not found']);
+            return json_encode([
+                "error" => true,
+                "message" => "Transaction not found",
+            ]);
         }
         return $result[0];
     }
--- script-sql/init.sql
+++ script-sql/init.sql
@@ -1,17 +1,26 @@
 CREATE database IF NOT EXISTS urubuDoPix;
-use urubuDoPix; 
 
-create table if not exists users(
+use urubuDoPix;
+
+create table if not exists users (
     id int auto_increment primary key,
     user_name varchar(35) not null,
     user_balance real
 );
 
-create table if not exists transactions(
+create table if not exists transactions (
     id int auto_increment primary key,
     userId int not null,
-    depositValue float not null, 
-    depositDate date not null, 
-    profitTransaction float,
-    FOREIGN KEY (userId) REFERENCES users(id)
+    depositValue float not null,
+    depositDate date not null,
+    FOREIGN KEY (userId) REFERENCES users (id)
+);
+
+create table if not exists actions (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    userId INT NOT NULL,
+    transactionId INT NOT NULL,
+    action VARCHAR(255) NOT NULL,
+    FOREIGN KEY (userId) REFERENCES users (id),
+    FOREIGN KEY (transactionId) REFERENCES transactions (id)
 );
